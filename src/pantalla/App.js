2c7fe2581884458a334a13f843bd629af8e7565e
PabloD97 <pablo97758@gmail.com>
2020-11-28 18:40:04 -0300

    Inicio de proycto: - feat: Se agrego la pantalla incial sin estilo.

---
+++ src/pantalla/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          Bienvenido al juego del piedra papel tijera lagarto o spock!
+          <p>Espero que se divierta :) </p>
+        </h1>
+      </div>
+      <div>
+        <button>
+          jugar contra la CPU
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
